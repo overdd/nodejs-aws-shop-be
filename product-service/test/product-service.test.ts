4571c9d959c81b1c35a3c1375b7d048806a917d9
overdd <antonsiarko@gmail.com>
2024-06-15 22:38:15 +0200

    oh, it's a candy shop, rigth

--- product-service/test/product-service.test.ts
+++ product-service/test/product-service.test.ts
@@ -12,9 +12,9 @@ describe('ProductService', () => {
       const products = productService.getProducts();
       expect(products).toHaveLength(3);
       expect(products).toEqual([
-        { id: 1, name: 'Product 1', description: 'This is product 1', price: 9.99 },
-        { id: 2, name: 'Product 2', description: 'This is product 2', price: 14.99 },
-        { id: 3, name: 'Product 3', description: 'This is product 3', price: 19.99 },
+        { id: 1, title: 'Candy 1', description: 'This is product 1', price: 9.99 },
+        { id: 2, title: 'Candy 2', description: 'This is product 2', price: 14.99 },
+        { id: 3, title: 'Candy 3', description: 'This is product 3', price: 19.99 },
       ]);
     });
   });
@@ -22,7 +22,7 @@ describe('ProductService', () => {
   describe('getProductById function', () => {
     it('should return the product with the given id', () => {
       const product = productService.getProductById('2');
-      expect(product).toEqual({ id: 2, name: 'Product 2', description: 'This is product 2', price: 14.99 });
+      expect(product).toEqual({ id: 2, title: 'Candy 2', description: 'This is product 2', price: 14.99 });
     });
 
     it('should return undefined if the product is not found', () => {
