34adc12458393bf9ebe8e9729ab2188df7b10c97
overdd <antonsiarko@gmail.com>
2024-06-29 21:10:38 +0200

    task 5, 2 new lambdas are ready to deploy

---
+++ import-service/lib/import-service-stack.ts
@@ -0,0 +1,73 @@
+import * as cdk from "aws-cdk-lib";
+import { Construct } from "constructs";
+import * as lambda from "aws-cdk-lib/aws-lambda";
+import * as s3 from "aws-cdk-lib/aws-s3";
+import * as apigateway from "aws-cdk-lib/aws-apigateway";
+import * as lambdaEventSources from 'aws-cdk-lib/aws-lambda-event-sources';
+import { CANDY_STORE_BUCKET } from "./src/support/constants";
+
+export class ImportServiceStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    const bucket = s3.Bucket.fromBucketName(
+      this,
+      CANDY_STORE_BUCKET.BUCKET_NAME,
+      CANDY_STORE_BUCKET.BUCKET_NAME
+    );
+
+    const importProductsFile = new lambda.Function(this, "ImportProductsFile", {
+      runtime: lambda.Runtime.NODEJS_20_X,
+      handler: "handlers/importProductsFileHandler.handler",
+      code: lambda.Code.fromAsset("dist", {
+        exclude: ["*.d.ts"],
+      }),
+      environment: {
+        BUCKET_NAME: bucket.bucketName,
+      },
+    });
+
+    const importFileParser = new lambda.Function(this, "ImportFileParser", {
+      runtime: lambda.Runtime.NODEJS_20_X,
+      handler: "handlers/importFileParserHandler.handler",
+      code: lambda.Code.fromAsset("dist", {
+        exclude: ["*.d.ts"],
+      }),
+      environment: {
+        BUCKET_NAME: bucket.bucketName,
+      },
+    });
+
+    importFileParser.addEventSource(
+      new lambdaEventSources.S3EventSource(bucket as s3.Bucket, {
+        events: [
+          s3.EventType.OBJECT_CREATED,
+          s3.EventType.OBJECT_REMOVED
+        ],
+        filters: [
+          { prefix: 'uploaded/' },
+        ],
+      })
+    );
+
+    bucket.grantReadWrite(importProductsFile);
+    bucket.grantReadWrite(importFileParser);
+
+    const api = new apigateway.RestApi(this, "ImportServiceApi", {
+      restApiName: "Import Service API",
+      description: "API for the Import Service",
+    });
+
+    const importResource = api.root.addResource("import");
+    const importMethod = importResource.addMethod(
+      'GET',
+      new apigateway.LambdaIntegration(importProductsFile),
+      {
+        requestParameters: {
+          'method.request.querystring.name': true,
+        },
+      }
+    );
+    
+  }
+}
---
+++ import-service/lib/src/handlers/importFileParserHandler.ts
@@ -0,0 +1,48 @@
+import { S3Event, S3EventRecord, S3Handler } from 'aws-lambda';
+import { S3Client, GetObjectCommand } from '@aws-sdk/client-s3';
+import csv from 'csv-parser'
+import { Readable } from 'stream';
+
+const s3Client = new S3Client({});
+
+export const handler: S3Handler = async (event: S3Event) => {
+  console.log('Received S3 event:', JSON.stringify(event, null, 2));
+
+  for (const record of event.Records) {
+    await processRecord(record);
+  }
+};
+
+async function processRecord(record: S3EventRecord) {
+  const { bucket, object } = record.s3;
+  const { name: bucketName, arn: bucketArn } = bucket;
+  const { key } = object;
+
+  console.log(`Processing object: ${key} from bucket: ${bucketName}`);
+
+  const getObjectCommand = new GetObjectCommand({
+    Bucket: bucketName,
+    Key: key,
+  });
+
+  const response = await s3Client.send(getObjectCommand);
+  const readableStream = response.Body as Readable;
+
+  return new Promise<void>((resolve, reject) => {
+    readableStream
+      .pipe(csv())
+      .on('data', (data: any) => {
+        console.log('Parsed record:', data);
+      })
+      .on('error', (err: any) => {
+        console.error('Error parsing CSV:', err);
+        reject(err);
+      })
+      .on('end', () => {
+        console.log('Finished processing object:', key);
+        resolve();
+      });
+  });
+}
+
+export default exports.handler;
